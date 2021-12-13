a9ae53dcfd4f48367c89c4333ed926c9449b7858
luckylion999 <try.best0007@gmail.com>
2021-12-13 06:49:34 -0500

    fix error on blacklist filter

--- keywords_api/views.py
+++ keywords_api/views.py
@@ -30,6 +30,7 @@ class KeywordsAPIView(APIView):
         result = []
         websites = websites.split(',')
         origin_keywords = origin_keywords.split(',')
+        blacklist = blacklist.split(',')
 
         for website in websites:
             if website.startswith('http'):
